833efcecc51dced8fefe8dbba015ffc8d425562a
tiffanykaye28 <tiffanykayearagon28@gmail.com>
2025-08-14 07:52:48 +0800

    added panel

--- src/app/banking.java
+++ src/app/banking.java
@@ -3,15 +3,37 @@ package app;
 
 public class banking {
     
-     int accountNo = 20013, pin = 456789;
-    String name;
-    float initializeBalance = 0;
+    private int accountNo, pin;
+    private float initializeBalance;
     
-    public boolean validateAccount(int accn, int pi){
-        
-         return accn == accountNo && pi == pin;
+    
+    public void setAccountNo(int accountNo){
+        this.accountNo = accountNo;
     }
     
+    public int getAccountNo(){
+        return accountNo;
+    }
+    
+    public void setPin(int pin){
+        this.pin = pin;
+    }
+    
+    public int getPin(){
+        return pin;
+    }
+   
+    public boolean validateAccount(int accn, int pi){
+        
+        if(accn == accountNo && pi == pin){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    
+ 
     public int listAccount(){
         return accountNo;
     }
@@ -21,7 +43,12 @@ public class banking {
     
     
     public void viewBalance(){
+        
+        
+        
+        
+        
+        
+        
     
-    
-    }
-}
+     
--- src/mainc/main.java
+++ src/mainc/main.java
@@ -9,58 +9,92 @@ public class main {
     
     public static void main(String[] args) {
         Scanner sr = new Scanner(System.in);
-        
-        
+
         System.out.println("HELLO WELCOME TO THE SYSTEM!");
-        System.out.println("What is your choice for today!?");
-        System.out.println("1.Banking");
-        System.out.println("2.Doctors Appointment");
-        System.out.println("3.Shopping");
-        
-        System.out.print("Enter Choice: ");
-        int choice = sr.nextInt();
-        
-        switch(choice){
-            case 1:
-                banking bs = new banking();
-                
-               int attempts = 3;
-               
-                while(attempts > 0){
-                    
-                    System.out.print("Enter Account No: ");
-                    int accountNo = sr.nextInt();
-                    
-                    System.out.println("Enter Pin: ");
-                    int pin = sr.nextInt();
-                    
-                    if(bs.validateAccount(accountNo, pin)){
-                        System.out.println("LOGIN SUCCESSFULLY");
+        System.out.println("What do you feel doing today? ");
+        System.out.println("1. Banking");
+        System.out.println("2. Doctors Appointment");
+        System.out.println("3. Shopping");
+           
+       banking bc[]  = new banking[10];
+       int index = 0;
+            System.out.println("Enter Choice: ");
+            int choice = sr.nextInt();
+            
+            switch(choice){
+                case 1:
+                    int resp;
+                     do {
+                System.out.println("1. Register Account");
+                System.out.println("2. Login Account");
+                System.out.println("3. View All Accounts");
+                System.out.println("Enter Selection: ");
+                int action = sr.nextInt();
+           
+                switch(action){
+                    case 1:
+                        if(index >= bc.length){
+                            System.out.println("Registration accounts reached");
+                            break;
+                        }
                         
+                        bc[index] = new banking();
+                        System.out.println("Enter Account No: ");
+                        bc[index].setAccountNo(sr.nextInt());
+                        
+                        System.out.println("Enter Pin: ");
+                        bc[index].setPin(sr.nextInt());
+                        
+                        System.out.println("Registration Successful");
+                        index++;
                         break;
                         
-                    }else{
-                        attempts--;
-                        System.out.println("LOGIN FAILED!");
-                        System.out.println("Remaning Attempts "+attempts);
+                    case 2:
+                        int attempts = 3;
+                        boolean found = false;
                         
-                        if(attempts ==0){
-                            System.out.println("Max Attempts reached.");
-                            System.out.println("Access Denied");
+                        while(attempts > 0){
+                            System.out.println("Enter Account No: ");
+                            int accn = sr.nextInt();
+                            
+                            System.out.println("Enter Pin: ");
+                            int pi = sr.nextInt();
+                            
+                            for(int acc = 0; acc < index; acc++){
+                            if(bc[acc].validateAccount(accn, pi)){
+                                System.out.println("LOGIN SUCCESSFUL");
+                                found = true;
+                                break;
+                                  } 
+                            }
+                            if(found){
+                                break;
+                            }else{
+                                attempts--;
+                                System.out.println("LOGIN FAILED!");
+                                System.out.println(attempts+" Attempts Remaining");
+                                
+                               if (attempts == 0){
+                                    System.out.println("Max Attempts Reached");
+                                    System.out.println("Access denied");
+                               }
+                            }
                         }
-                    }
-                        
+                        break;
+                    case 3:
+                        break;
                 }
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            default:
-                System.out.println("Invalid! Section.");
-                System.out.println("Try again.");
+                        System.out.println("Do you want to continue? (Y = 1/N = 0)");
+                        resp = sr.nextInt();
+                }while(resp == 1);
+                  break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    System.out.println("Invalid Selection!");
+                    System.out.println("Try again.");
+            }
         }
-    }
-    
-    
-}
+      }        
